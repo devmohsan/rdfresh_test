0b03c560c5367d20aa36b2e466a0cd660402b271
mohsan code <mohsancode@gmail.com>
2026-04-21 13:57:19 +0500

    minor change

--- firebase/db.js
+++ firebase/db.js
@@ -1,6 +1,7 @@
 // firebase/db.js
 const admin = require('firebase-admin');
 const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
